6b21c439ac91e2adcf2a8c0ea5162907ab929464
underbleu <underbleu.dev@gmail.com>
2019-08-09 15:26:01 +0900

    Add message type for rawTransaction

--- src/components/Message.js
+++ src/components/Message.js
@@ -13,12 +13,14 @@ const Message = ({
         'Message--error': type === 'error',
         'Message--txHash': type === 'txHash',
         'Message--receipt': type === 'receipt',
+        'Message--rawTransaction': type === 'rawTransaction',
       })}
     >
       <div className="Message__status">
         {type === 'error' && 'error'}
         {type === 'txHash' && 'txHash'}
         {type === 'receipt' && 'receipt'}
+        {type === 'rawTransaction' && `SignedTransaction`}
       </div>
       <div className="Message__message">
         {message}
